2a7825bed7a6f27391d6cf5bf46d472afbc3b6ee
GreenCitrus6 <DanielAAad01@gmail.com>
2024-04-05 16:53:04 -0700

    Replaced README.md with frontend mentor placeholder README

--- src/app/mapdisplay.js
+++ src/app/mapdisplay.js
@@ -1,5 +1,18 @@
 'use client';
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import locationArrow from './../../public/static/images/icon-location.svg';
+import { Icon } from 'leaflet';
+
+const arrowIcon = new L.Icon({
+    iconUrl: locationArrow,
+    iconRetinaUrl: locationArrow,
+    iconSize: [32,32],
+    shadowSize: [50,64],
+    iconAnchor: [22,94],
+    shadowAnchor: [4,62],
+    popupAnchor: [-3,-76]
+})
+
 
 export default function MapDisplay() {
     return(
@@ -9,7 +22,7 @@ export default function MapDisplay() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
+                <Marker position={[51.505, -0.09]} icon={arrowIcon}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
